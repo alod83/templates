41bc4dc6b8cd54687578f30ac4c37a6d3c8d3098
Angelica Lo Duca <angelica@MacBook-Pro-di-Angelica.local>
2016-06-28 12:32:23 +0200

    added get_input() to php utilities

--- php/utilities/utilities.php
+++ php/utilities/utilities.php
@@ -2,15 +2,49 @@
 
 /* Connection to a mysql database. This function returns 
  * the reference to the connection. */
-function mysqlconnect($user, $password,$db_name)
+function mysqlconnect($user, $password,$db_name = null)
 {
 	$conn = mysqli_connect("localhost",$user, $password);
 
 	if(!$conn) die("Connection Error".mysqli_error()); 
 
-	$ok = mysqli_select_db($conn ,$db_name);
-	if(!$ok) die("Error while selecting db $db_name".mysql_errno()); 
+	if(!is_null($db_name))
+	{
+		$ok = mysqli_select_db($conn ,$db_name);
+		if(!$ok) die("Error while selecting db $db_name".mysql_errno()); 
+	}
 	return $conn;
+}
+
+/*
+ * This function parses the stdin.
+ * $opts should be in a form similar to 's:n:f:l:h'
+ */
+function get_input($opts)
+{
+	// WARNING: this function works only with inputs having a value
+	// remove last : to avoid the final empty string
+	$opts_array = explode(":",substr($opts, 0, strlen($opts)-1));
+		
+	$check = false;
+	$input = getopt($opts);
+	
+	// check whether the array is empty
+	if(empty($input))
+	{
+		$check = true;
+	}
+	
+	// check whether all the parameters have been set
+	foreach ($opts_array as $opt)
+		if(!in_array($opt, array_keys($input))) $check = true;
+	
+	if($check)
+	{
+		print_help(); // define print_help() in your code
+		exit;
+	}
+	return $input;
 }
 
 ?>
