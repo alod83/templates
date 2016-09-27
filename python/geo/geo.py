54bae3789460a0ed36cd636b6887a5ba2eca7fd6
Angelica Lo Duca <angelica@MacBook-Pro-di-Angelica.local>
2016-09-27 17:38:47 +0200

    updated script to calculate position in a grid

--- python/geo/geo.py
+++ python/geo/geo.py
@@ -1,17 +1,23 @@
 import numpy as np
 from math import floor
+from math import copysign
 
-def get_position(x,cx,x_min):
+def get_position(x,cx,type):
     if cx > 0:
-        xg = floor((x - x_min)/cx)+1
+        if type == "lng":
+            xg = copysign(floor((abs(x)/cx)%180),x)
+            if xg == -0:
+                xg = 0 
+        else:
+            xg = floor(x/cx)
     else:
         xg = -1
     return xg
 
-# (x - x1)/cx + x1
+# x/cx + x1
 # get position in the grid
-def get_position_in_grid(x, y, cx, cy, x_min, y_min):
-    xg = get_position(x,cx,x_min)
-    yg = get_position(y,cy,y_min)
+def get_position_in_grid(x, y, cx, cy):
+    xg = get_position(x,cx,"lng")
+    yg = get_position(y,cy,"lat")
     return np.array([xg, yg])
 
